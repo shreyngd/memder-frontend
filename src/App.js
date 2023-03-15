efda28ce027fb0e7cbb93891c68cfa02e1dec74b
Avin Patel <avinp@192.168.1.6>
2023-03-15 13:01:27 +0530

    Changed the text of link

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Learn Chakra
+              Memder
             </Link>
           </VStack>
         </Grid>
